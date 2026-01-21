de33ec87f084aa8d39dc865ee96143c20bc867fa
i am Chuppyy <62337417+chuppyy@users.noreply.github.com>
2026-01-21 10:44:28 +0700

    Update domain config via WinForms tool

--- domain.tsx
+++ domain.tsx
@@ -1,3 +1,3 @@
-export const domain = "https://vtus.cafex.biz";
+//export const domain = "https://vtus.cafex.biz";
 //export const domain = "https://dailynewsus.feji.io";
-//export const domain = "https://daily.livextop.com";
+export const domain = "https://daily.livextop.com";
