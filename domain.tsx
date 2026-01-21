2e90e6937c764f4e41e2722a1a327c56dbaaf96f
i am Chuppyy <62337417+chuppyy@users.noreply.github.com>
2026-01-21 08:13:59 +0700

    Update domain URL to new endpoint

--- domain.tsx
+++ domain.tsx
@@ -1,2 +1,3 @@
-export const domain = "https://dailynewsus.feji.io";
+export const domain = "https://vtus.cafex.biz";
+//export const domain = "https://dailynewsus.feji.io";
 //export const domain = "https://daily.livextop.com";
